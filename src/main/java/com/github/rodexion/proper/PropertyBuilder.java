4c70a36f6a24706b0f866219e6bf0405c5759495
moiseev <moiseev@infoscience.co.jp>
2013-10-08 18:48:14 +0900

    Support for custom attributes

--- src/main/java/com/github/rodexion/proper/PropertyBuilder.java
+++ src/main/java/com/github/rodexion/proper/PropertyBuilder.java
@@ -20,54 +20,61 @@
 
 package com.github.rodexion.proper;
 
-import static com.github.rodexion.proper.util.Preconditions.checkNotNull;
-
 import java.util.Collections;
+import java.util.Map;
+
+import static com.github.rodexion.proper.util.Preconditions.checkNotNull;
 
 /**
  * @author rodexion
  * @since 0.1
  */
 public class PropertyBuilder<T> {
-  private final Proper.Info<T> info;
-  private Converter<T> converter = Converters.voidConverter();
-  private Validator<T> validator = Validators.voidValidator();
-  private PropertyListener propertyListener = PropertyListeners.voidListener();
+    private final Proper.Info<T> info;
+    private Converter<T> converter = Converters.voidConverter();
+    private Validator<T> validator = Validators.voidValidator();
+    private PropertyListener propertyListener = PropertyListeners.voidListener();
+    private Map<String, Object> attributes = Collections.emptyMap();
 
-  PropertyBuilder(String key, Class<T> typeClass, T defaultValue) {
-    this.info = new Proper.Info<>(key,
-            typeClass,
-            defaultValue,
-            Collections.<String, Object>emptyMap());
-  }
+    PropertyBuilder(String key, Class<T> typeClass, T defaultValue) {
+        this.info = new Proper.Info<>(key,
+                typeClass,
+                defaultValue,
+                attributes);
+    }
 
-  public PropertyBuilder<T> converter(Converter<T> converter) {
-    this.converter = checkNotNull("converter", converter);
-    return this;
-  }
+    public PropertyBuilder<T> converter(Converter<T> converter) {
+        this.converter = checkNotNull("converter", converter);
+        return this;
+    }
 
-  public PropertyBuilder<T> converterProvider(ConverterProvider converterProvider) {
-    this.converter = checkNotNull("converterProvider", converterProvider).getConverter(info.getType());
-    return this;
-  }
+    public PropertyBuilder<T> converterProvider(ConverterProvider converterProvider) {
+        this.converter = checkNotNull("converterProvider", converterProvider).getConverter(info.getType());
+        return this;
+    }
 
-  public PropertyBuilder<T> validator(Validator<T> validator) {
-    this.validator = checkNotNull("validator", validator);
-    return this;
-  }
+    public PropertyBuilder<T> validator(Validator<T> validator) {
+        this.validator = checkNotNull("validator", validator);
+        return this;
+    }
 
 
-  public PropertyBuilder<T> validatorProvider(ValidatorProvider validatorProvider) {
-    this.validator = checkNotNull("validatorProvider", validatorProvider).getValidator(info.getType());
-    return this;
-  }
+    public PropertyBuilder<T> validatorProvider(ValidatorProvider validatorProvider) {
+        this.validator = checkNotNull("validatorProvider", validatorProvider).getValidator(info.getType());
+        return this;
+    }
 
-  public PropertyBuilder<T> propertyListener(PropertyListener propertyListener) {
-    this.propertyListener = checkNotNull("propertyListener", propertyListener);
-    return this;
-  }
+    public PropertyBuilder<T> propertyListener(PropertyListener propertyListener) {
+        this.propertyListener = checkNotNull("propertyListener", propertyListener);
+        return this;
+    }
 
-  public Proper.Ty<T> build() {
-    return new Proper.Ty<T>(info, converter, validator, propertyListener);
-  }
+    public PropertyBuilder<T> attributes(Map<String, Object> attributes) {
+        this.attributes = checkNotNull("attributes", attributes);
+        return this;
+    }
+
+    public Proper.Ty<T> build() {
+        return new Proper.Ty<>(info, converter, validator, propertyListener, attributes);
+    }
 }
--- src/test/java/com/github/rodexion/proper/Examples.java
+++ src/test/java/com/github/rodexion/proper/Examples.java
@@ -1,117 +1,131 @@
 package com.github.rodexion.proper;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-
+import org.fest.assertions.data.MapEntry;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.fest.assertions.api.Assertions.assertThat;
 
 /**
  * @author rodexion
  * @since 0.1
  */
 public class Examples {
-  @Rule
-  public TestRule tmpProps = RuleUtils.tmpSysProp("my.key");
+    @Rule
+    public TestRule tmpProps = RuleUtils.tmpSysProp("my.key");
 
-  @SuppressWarnings("UnusedDeclaration")
-  enum MyEnum {
-    One, Two, Three;
-  }
-
-  /**
-   * Your are recommended to define your own static property accessor.
-   * <p/>
-   * In the accessor you can set up your own default converters, validators
-   * and property listeners.
-   */
-  static class MyProper {
-    static final List<String> messages = new ArrayList<>();
-
-    public static <T> PropertyBuilder<T> ty(String key, T defaultValue) {
-      return Proper.tyBuilder(key, defaultValue)
-              //Register a custom property listener
-              .propertyListener(new PropertyListeners.BasePropertyListener() {
-                @Override
-                public void notFound(Proper.Info<?> info) {
-                  messages.add("Not found " + info.getKey() + " using default " + info.getDefaultValue());
-                }
-
-                @Override
-                public void conversionFailed(String value, String conversionError, Proper.Info<?> info) {
-                  messages.add("Could not convert " + value + " to int");
-                }
-
-                @Override
-                public void validationAfterConversionFailed(Object value, String validationError, Proper.Info<?> info) {
-                  messages.add(validationError);
-                }
-              })
-                      //Setup custom converters
-              .converterProvider(ConverterProviders.builder()
-                      //Add converters for your custom types (btw, enums are converted by default!)
-                      .add(new Converters.BaseConverter<MyEnum>(MyEnum.class) {
-                        @Override
-                        protected MyEnum doConvert(String value, Proper.Info<MyEnum> info) {
-                          return MyEnum.valueOf(value);
-                        }
-                      })
-                              //Add all the default converters if (primitive types, BigDecimal, Enums, etc.)
-                      .allFrom(ConverterProviders.defaultConverterProvider())
-                      .build())
-                      //Setup custom common validation
-              .validatorProvider(ValidatorProviders.builder()
-                      .add(new Validators.BaseValidator<MyEnum>(MyEnum.class) {
-                        @Override
-                        public Result afterConversion(MyEnum value, Proper.Info<MyEnum> info) {
-                          return value == MyEnum.One ? ok() : fail("value has to be 'One'");
-                        }
-                      }).build()
-              );
+    @SuppressWarnings("UnusedDeclaration")
+    enum MyEnum {
+        One, Two, Three;
     }
-  }
 
-  @Test
-  public void basicUsage() {
-    //Consider "my.key" not set
-    Proper.Ty<Integer> intProp = MyProper.ty("my.key", 123).build();
-    assertThat(intProp.getValue()).isEqualTo(123);
-    assertThat(MyProper.messages.contains("Not found my.key using default 123"));
-    MyProper.messages.clear();
+    /**
+     * Your are recommended to define your own static property accessor.
+     * <p/>
+     * In the accessor you can set up your own default converters, validators
+     * and property listeners.
+     */
+    static class MyProper {
+        static final List<String> messages = new ArrayList<>();
 
-    //Set it to a correct value
-    System.setProperty("my.key", "234");
-    assertThat(intProp.getValue()).isEqualTo(234);
-    assertThat(MyProper.messages).isEmpty();
+        public enum Unit {
+            Int, String
+        }
 
-    //Set it to a wrong value
-    System.setProperty("my.key", "xyz");
-    assertThat(intProp.getValue()).isEqualTo(123);//default
-    assertThat(MyProper.messages.contains("Could not convert xyz to int"));
-    MyProper.messages.clear();
-  }
+        public static <T> PropertyBuilder<T> ty(String key, T defaultValue, String description, Unit unit) {
+            Map<String, Object> attrs = new HashMap<>();
+            attrs.put("description", description);
+            attrs.put("unit", unit);
+            return Proper.tyBuilder(key, defaultValue)
+                    //Register a custom property listener
+                    .propertyListener(new PropertyListeners.BasePropertyListener() {
+                        @Override
+                        public void notFound(Proper.Info<?> info) {
+                            messages.add("Not found " + info.getKey() + " using default " + info.getDefaultValue());
+                        }
 
-  @Test
-  public void validationExample() {
-    Proper.Ty<Long> longProp = MyProper.ty("my.key", 123L)
-            .validator(Validators.<Long>comparableValidatorBuilder()
-                    .min(0L)
-                    .max(1000L)
-                    .build())
-            .build();
+                        @Override
+                        public void conversionFailed(String value, String conversionError, Proper.Info<?> info) {
+                            messages.add("Could not convert " + value + " to int");
+                        }
 
-    //Try a valid value
-    System.setProperty("my.key", "999");
-    assertThat(longProp.getValue()).isEqualTo(999L);
-    assertThat(MyProper.messages).isEmpty();
+                        @Override
+                        public void validationAfterConversionFailed(Object value, String validationError, Proper.Info<?> info) {
+                            messages.add(validationError);
+                        }
+                    })
+                            //Setup custom converters
+                    .converterProvider(ConverterProviders.builder()
+                            //Add converters for your custom types (btw, enums are converted by default!)
+                            .add(new Converters.BaseConverter<MyEnum>(MyEnum.class) {
+                                @Override
+                                protected MyEnum doConvert(String value, Proper.Info<MyEnum> info) {
+                                    return MyEnum.valueOf(value);
+                                }
+                            })
+                                    //Add all the default converters if (primitive types, BigDecimal, Enums, etc.)
+                            .allFrom(ConverterProviders.defaultConverterProvider())
+                            .build())
+                            //Setup custom common validation
+                    .validatorProvider(ValidatorProviders.builder()
+                            .add(new Validators.BaseValidator<MyEnum>(MyEnum.class) {
+                                @Override
+                                public Result afterConversion(MyEnum value, Proper.Info<MyEnum> info) {
+                                    return value == MyEnum.One ? ok() : fail("value has to be 'One'");
+                                }
+                            }).build()
+                    )
+                    .attributes(attrs);
+        }
+    }
 
-    //Try value out of valid scope
-    System.setProperty("my.key", "1001");
-    assertThat(longProp.getValue()).isEqualTo(123L);//default
-    assertThat(MyProper.messages).contains("1001 is greater than 1000");
-    MyProper.messages.clear();
-  }
+    @Test
+    public void basicUsage() {
+        //Consider "my.key" not set
+        Proper.Ty<Integer> intProp = MyProper.ty("my.key", 123, "My integer", MyProper.Unit.Int).build();
+        assertThat(intProp.getAttributes()).contains(
+                MapEntry.entry("description", "My integer"),
+                MapEntry.entry("unit", MyProper.Unit.Int));
+        assertThat(intProp.getValue()).isEqualTo(123);
+        assertThat(MyProper.messages.contains("Not found my.key using default 123"));
+        MyProper.messages.clear();
+
+        //Set it to a correct value
+        System.setProperty("my.key", "234");
+        assertThat(intProp.getValue()).isEqualTo(234);
+        assertThat(MyProper.messages).isEmpty();
+
+        //Set it to a wrong value
+        System.setProperty("my.key", "xyz");
+        assertThat(intProp.getValue()).isEqualTo(123);//default
+        assertThat(MyProper.messages.contains("Could not convert xyz to int"));
+        MyProper.messages.clear();
+    }
+
+    @Test
+    public void validationExample() {
+        Proper.Ty<Long> longProp = MyProper.ty("my.key", 123L, "My integer", MyProper.Unit.Int)
+                .validator(Validators.<Long>comparableValidatorBuilder()
+                        .min(0L)
+                        .max(1000L)
+                        .build())
+                .build();
+
+        //Try a valid value
+        System.setProperty("my.key", "999");
+        assertThat(longProp.getValue()).isEqualTo(999L);
+        assertThat(MyProper.messages).isEmpty();
+
+        //Try value out of valid scope
+        System.setProperty("my.key", "1001");
+        assertThat(longProp.getValue()).isEqualTo(123L);//default
+        assertThat(MyProper.messages).contains("1001 is greater than 1000");
+        MyProper.messages.clear();
+    }
 }
